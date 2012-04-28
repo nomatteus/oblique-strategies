ba0c0f10dc67938c5c16efd9c259492de6d6240a
nomatteus <matt@matthewruten.com>
2012-04-28 15:44:59 -0400

    Implement all basic app functionality, and partial styling

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,87 @@
+var Helpers = {
+  item_is_in_array: function(item, array) {
+    var alength = array.length;
+    for (var i=0; i < alength; i++) {
+      if (array[i] === item) {
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
 var ObliqueStrategies = function() {
   var strategies = ["A line has two sides", "Abandon desire", "Abandon normal instructions", "Accept advice", "Adding on", "Always the first steps", "Ask people to work against their better judgement", "Ask your body", "Be dirty", "Be extravagant", "Be less critical", "Breathe more deeply", "Bridges -build -burn", "Change ambiguities to specifics", "Change nothing and continue consistently", "Change specifics to ambiguities", "Consider transitions", "Courage!", "Cut a vital connection", "Decorate", "Destroy nothing; Destroy the most important thing", "Discard an axiom", "Disciplined self-indulgence", "Discover your formulas and abandon them", "Display your talent", "Distort time", "Do nothing for as long as possible", "Do something boring", "Do something sudden", "Do the last thing first", "Do the words need changing?", "Don't avoid what is easy", "Don't break the silence", "Don't stress one thing more than another", "Emphasize differences", "Emphasize the flaws", "Faced with a choice", "Find a safe part and use it as an anchor", "Give the game away", "Give way to your worst impulse", "Go outside. Shut the door.", "Go outside. Shut the door.", "Go to an extreme", "How would someone else do it?", "How would you have done it?", "In total darkness", "Is it finished?", "Is something missing?", "Is the style right?", "It is simply a matter or work", "Just carry on", "Listen to the quiet voice", "Look at the order in which you do things", "Magnify the most difficult details", "Make it more sensual", "Make what's perfect more human", "Move towards the unimportant", "Not building a wall; making a brick", "Once the search has begun", "Only a part", "Only one element of each kind", "Openly resist change", "Pae White's non-blank graphic metacard", "Question the heroic", "Remember quiet evenings", "Remove a restriction", "Repetition is a form of change", "Retrace your steps", "Reverse", "Simple Subtraction", "Slow preparation", "State the problem as clearly as possible", "Take a break", "Take away the important parts", "The inconsistency principle", "The most easily forgotten thing is the most important", "Think - inside the work -outside the work", "Tidy up", "Try faking it (from Stewart Brand)", "Turn it upside down", "Use 'unqualified' people", "Use an old idea", "Use cliches", "Use filters", "Use something nearby as a model", "Use your own ideas", "Voice your suspicions", "Water", "What context would look right?", "What is the simplest solution?", "What mistakes did you make last time?", "What to increase? What to reduce? What to maintain?", "What were you really thinking about just now?", "What would your closest friend do?", "What wouldn't you do?", "When is it for?", "Where is the edge?", "Which parts can be grouped?", "Work at a different speed", "Would anyone want it?", "Your mistake was a hidden intention"],
       num_strategies = strategies.length,
+      current_strategy,
+      $obstrat = $("#oblique-strategy"),
+      $faves = $("ul#faves"),
   init = function() {
+      addListeners();
+      display_random();
+      $("body").addClass("one-strategy");
+  },
+  add_fave = function() {
+    // Add strategy to local store list
     if (Modernizr.localstorage) {
-      var obstrat;
-      obstrat = localStorage.getItem('oblique_strategies');
-      console.log(JSON.parse(obstrat));
-      if (obstrat === null) {
-        obstrat = localStorage.setItem('oblique_strategies', JSON.stringify(strategies));
+      var faves = get_faves();
+      // Add current strategy to faves list
+      if (!Helpers.item_is_in_array(current_strategy, faves)) {
+        faves.push(current_strategy);
       }
+      // Save updated list in local storage
+      obstrat = localStorage.setItem('oblique_strategies_faves', JSON.stringify(faves));
     }
+    console.log("called faves... localStorage item `oblique_strategies_faves` is now: ");
+    console.log(localStorage.getItem('oblique_strategies_faves'));
+  },
+  get_faves = function() {
+    console.log("get faves... localStorage item `oblique_strategies_faves` is: ");
+    console.log(localStorage.getItem('oblique_strategies_faves'));
+    return JSON.parse(localStorage.getItem('oblique_strategies_faves')) || [];
+  },
+  clear_faves = function() {
+    localStorage.removeItem('oblique_strategies_faves');
+    console.log("clearing faves... localStorage item `oblique_strategies_faves` is now: ");
+    console.log(localStorage.getItem('oblique_strategies_faves'));
   },
   random_strategy = function() {
     rand_strategy_index = Math.floor(Math.random()*num_strategies);
     return strategies[rand_strategy_index];
+  },
+  display_random = function() {
+    current_strategy = random_strategy();
+    $obstrat.text(current_strategy);
+  },
+  populate_faves = function() {
+    var faves = get_faves(),
+        flength = faves.length;
+    $faves.html(""); // Clear any HTML
+    if (flength === 0) {
+      $faves.html("No faves yet. Why don't you add some?");
+    }
+    for (var i=0; i < flength; i++) {
+      $faves.append("<li>" + faves[i] + "</li>");
+    }
+  },
+  addListeners = function() {
+    $("#strat-nav #random a").live("click", function(){
+      $("body").addClass("one-strategy");
+      $("body").removeClass("strategy-list");
+      display_random();
+    });
+    $("#strat-nav #fave a").live("click", function(){
+      add_fave();
+    });
+    $("#strat-nav #view-faves a").live("click", function(){
+      $("body").addClass("strategy-list");
+      $("body").removeClass("one-strategy");
+      populate_faves();
+    });
+    $("#strat-nav #clear-faves a").live("click", function(){
+      clear_faves();
+      populate_faves();
+    });
   };
   init();
   return {
@@ -24,8 +92,6 @@ var ObliqueStrategies = function() {
 };
 
 var obstrat = ObliqueStrategies();
-
-$("#oblique-strategy").text(obstrat.get_random());
 
 var randr = Math.floor((Math.random()*255)+1),
     randg = Math.floor((Math.random()*255)+1),
