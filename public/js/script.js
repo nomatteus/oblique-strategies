90fd640f8103a0587af9ff15baa16e7b5f41a3dd
nomatteus <matt@matthewruten.com>
2012-05-13 02:28:58 -0400

    Remove extraneous JS files

--- public/js/script.js
+++ public/js/script.js
@@ -147,6 +147,10 @@ Display = {
   }
 };
 
-
 var obstrat = ObliqueStrategies();
 
+
+// plugins.js
+// make it safe to use console.log always
+(function(b){function c(){}for(var d="assert,clear,count,debug,dir,dirxml,error,exception,firebug,group,groupCollapsed,groupEnd,info,log,memoryProfile,memoryProfileEnd,profile,profileEnd,table,time,timeEnd,timeStamp,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
+{console.log();return window.console;}catch(err){return window.console={};}})());
